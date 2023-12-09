8db59e2fb1e3ba2dca0807384960010cbf292ad5
Victor Naslund Cuesta <vnaslund@c3r1s6.42malaga.com>
2023-12-09 16:09:16 +0100

    cpp03:ex01 done

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 14:33:46 by vnaslund          #+#    #+#             */
+/*   Updated: 2023/12/09 15:48:02 by vnaslund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap(std::string name);
+		~ScavTrap();
+		void	guardGate();
+};
+
+
+#endif
---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 13:47:27 by vnaslund          #+#    #+#             */
+/*   Updated: 2023/12/09 16:04:36 by vnaslund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int	main(void)
+{
+	std::string		input;
+	std::string		opponent;
+	unsigned int	amount;
+
+	std::cout << "Welcome to ScavTrap fight simulator\n" 
+			  << "What do you want to name your fighter > ";
+
+	getline(std::cin, input);
+	ScavTrap fighter(input);
+
+	std::cout << "What do you want to name your opponent > ";
+	getline(std::cin, opponent);
+	
+	std::cout << "Starting fight simulation...\nValid actions are ATTACK, TAKEDAMAGE, REPAIR, GUARD & EXIT" << std::endl;
+	while (1)
+	{
+		std::cout << "Action > ";
+		getline(std::cin, input);
+		
+		if (input == "ATTACK")
+			fighter.attack(opponent);
+		else if (input == "GUARD")
+			fighter.guardGate();
+		else if (input == "TAKEDAMAGE")
+		{
+			std::cout << "How much damage do you want to take? > ";
+			while (!(std::cin >> amount) || amount < 0)
+			{
+				std::cout << "Invalid input, only unsigned ints allowed" << std::endl;
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "How much damage do you want to take? > ";
+			}
+			fighter.takeDamage(amount);
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		else if (input == "REPAIR")
+		{
+			std::cout << "How many hitpoints do you want to add? > ";
+			while (!(std::cin >> amount) || amount < 0)
+			{
+				std::cout << "Invalid input, only unsigned ints allowed" << std::endl;
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "How many hitpoints do you want to add? > ";
+			}
+			fighter.beRepaired(amount);
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		else if (input == "EXIT")
+		{
+			std::cout << "Exiting program" << std::endl;
+			break ;
+		}
+		else
+			std::cout << "Invalid input🤡" << std::endl;
+	}
+}
