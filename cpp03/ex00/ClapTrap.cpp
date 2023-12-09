30df61d9e346a352b58b09bc212945ae61f764b3
Victor Naslund Cuesta <vnaslund@c3r1s6.42malaga.com>
2023-12-09 14:24:21 +0100

    cpp03:ex00 done

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 13:25:52 by vnaslund          #+#    #+#             */
+/*   Updated: 2023/12/09 14:11:36 by vnaslund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+	_hitPoints = 10;
+	_energyPoints = 10;
+	_attackDamage = 0;
+	_name = name;
+	std::cout << "ClapTrap constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+void	ClapTrap::attack(const std::string& target)
+{
+	if (!_energyPoints)
+	{
+		std::cout << "ClapTrap " << _name 
+		<< " doesn't have enough energy points to attack" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << _name << " attacks " << target
+	<< ", causing " << _attackDamage << " points of damage!" << std::endl;
+	_energyPoints--;
+}
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << _name << " takes "
+	<< amount << " points of damage!" << std::endl;
+	_hitPoints -= amount;
+	if (_hitPoints < 1)
+	{
+		std::cout << "ClapTrap " << _name << " has died due to having "
+		<< _hitPoints << " hitpoints💀" << std::endl;
+	}
+}
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (!_energyPoints)
+	{
+		std::cout << "ClapTrap " << _name 
+		<< " doesn't have enough energy points to repair itself" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << _name << " repairs itself to increase it's hitpoints from "
+	<< _hitPoints << " >> " << _hitPoints + amount << std::endl;
+	_energyPoints--;
+	_hitPoints += amount;
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnaslund <vnaslund@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 13:22:26 by vnaslund          #+#    #+#             */
+/*   Updated: 2023/12/09 13:50:43 by vnaslund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap();
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+	
+	private:
+		int		_hitPoints;
+		int		_energyPoints;
+		int		_attackDamage;
+		std::string	_name;
+};
+
+#endif
